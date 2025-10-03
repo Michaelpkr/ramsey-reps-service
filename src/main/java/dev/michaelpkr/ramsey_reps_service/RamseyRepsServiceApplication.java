d84ea98e644e23458d5b09c263992f5d40379ebc
Michael Parker <michael.parker@mm-parkerm2.peace.daveramsey.com>
2025-10-03 12:29:12 -0500

    Initial commit

---
+++ src/main/java/dev/michaelpkr/ramsey_reps_service/RamseyRepsServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.michaelpkr.ramsey_reps_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamseyRepsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RamseyRepsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/michaelpkr/ramsey_reps_service/RamseyRepsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.michaelpkr.ramsey_reps_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RamseyRepsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
